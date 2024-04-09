6843272877cee85b3d882664bf362e258d648cfc
Felix s1910238061 <s2210629018@fhooe.at>
2024-04-09 11:37:22 +0200

    reduced to a minimum

--- Assets/Scripts/ContourFinderDepthMap.cs
+++ Assets/Scripts/ContourFinderDepthMap.cs
@@ -11,19 +11,15 @@ public class ContourFinderDepthMap : WebCamera
 {
     private Mat image;
     private int frameCount = 0;
-    private int processEveryNthFrame = 2; // Change this to process every nth frame
+
 
     protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)
     {
         frameCount++;
-        if (frameCount % processEveryNthFrame == 0)
-        {
-            image = OpenCvSharp.Unity.TextureToMat(input);
-            Cv2.Flip(image, image, FlipMode.Y);
-            Cv2.CvtColor(image, image, ColorConversionCodes.BGR2GRAY);
-            // half the resolution
-            Cv2.Resize(image, image, new OpenCvSharp.Size(image.Width / 2, image.Height / 2));
-        }
+        
+        image = OpenCvSharp.Unity.TextureToMat(input);
+            
+        
 
         if(output == null)
         {
