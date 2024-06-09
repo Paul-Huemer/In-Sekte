24960e84b6a7ee62be3ceeb44f09b6ca38d13e71
Paul <paul.huemer01@gmail.com>
2024-06-09 15:04:44 +0200

    the sounds are now sounding

--- Assets/Scripts/BackgroundAudioManager.cs
+++ Assets/Scripts/BackgroundAudioManager.cs
@@ -1,21 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundAudioManager : MonoBehaviour
+public class AudioManager : MonoBehaviour
 {
-    public AudioSource audioSource;
+    private static AudioManager instance;
 
-    // Start is called before the first frame update
-    void Start()
+    public static AudioManager Instance
     {
-
-        
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<AudioManager>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = "AudioManager";
+                    instance = obj.AddComponent<AudioManager>();
+                }
+            }
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public AudioSource musicSource;
+    public AudioClip[] musicClips;
+    private int currentClipIndex = 0;
+
+    void Awake()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        musicSource.loop = true;
+        PlayNextClip();
+    }
+
+    public void PlayNextClip()
+    {
+        if (musicClips.Length == 0)
+            return;
+
+        musicSource.clip = musicClips[currentClipIndex];
+        musicSource.Play();
+
+        currentClipIndex = (currentClipIndex + 1) % musicClips.Length;
     }
 }
